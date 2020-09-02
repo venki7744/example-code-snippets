1aac433d28db20a9a99b22c29ad4dce308831119
venki7744 <36258133+venki7744@users.noreply.github.com>
2020-09-02 22:37:48 +0530

    Add files via upload

--- python-jdbc-sqlalchemy/src/readPostgresOrg.py
+++ python-jdbc-sqlalchemy/src/readPostgresOrg.py
@@ -8,15 +8,17 @@ from sqlalchemy import event
 
 print(os.environ["PG_JDBC_DRIVER_PATH"])#="D:\Learn\ApacheBeam\jar\postgresql-42.2.16.jar"
 
-con=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest')).execution_options(isolation_level='AUTOCOMMIT')
+con=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'),driver_path="D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar").execution_options(isolation_level='AUTOCOMMIT')
 con=con.raw_connection()
-wcon=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'))
+wcon=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'),driver_path="D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar")
 @event.listens_for(wcon,"before_cursor_execute")
 def receive_before_cursor_execute(
        conn, cursor, statement, params, context, executemany
         ):
-            if executemany:
-                cursor.fast_executemany = True
+    print("insideTrigger")
+    if executemany:
+        print("insideexecutemany")
+        cursor.fast_executemany = True
 
 print('starttime-{}'.format(datetime.datetime.now()))
 for readChunk in pd.read_sql("select randomText,randomNumber from public.testtable",con,chunksize=200000):
@@ -25,6 +27,6 @@ for readChunk in pd.read_sql("select randomText,randomNumber from public.testtab
     print("count:{}".format(len(readChunk)))
     print('StartLoadChunk-{}'.format(datetime.datetime.now()))
     #readChunk.to_sql('testtableout1',wcon,schema='public',if_exists='append',index=False)
-    readChunk.to_sql('testtableout2',wcon,schema='public',if_exists='append',index=False,chunksize=15000,method='multi')
+    readChunk.to_sql('testtableout10',wcon,schema='public',if_exists='append',index=False,chunksize=15000,method='multi')
     print('EndLoadChunk-{}'.format(datetime.datetime.now()))
 print('Endtime-{}'.format(datetime.datetime.now()))
