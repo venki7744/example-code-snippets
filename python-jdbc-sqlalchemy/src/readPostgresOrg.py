5f2e62341a0f52e19278e90d4817f8f5bace91f8
venki7744 <36258133+venki7744@users.noreply.github.com>
2020-09-04 08:04:39 +0530

    Add files via upload
    Removed sqlalchemy dependencies

--- python-jdbc-sqlalchemy/src/readPostgresOrg.py
+++ python-jdbc-sqlalchemy/src/readPostgresOrg.py
@@ -6,27 +6,76 @@ import os
 import datetime
 from sqlalchemy import event
 
+def fix_values(values):
+    outvalues=[]
+    for value in values:
+        if value is None:
+            value='NULL'
+
+        if isinstance(value,(int,float)):
+            if math.isnan(value):
+                value='NULL'
+            value=str(value)
+        else:
+            value="'{}'".format(str(value).replace("'","''"))
+        outvalues.append(value)
+    return outvalues
+
+def insert_sql_build(values:list,destschema,desttable,columns):
+    finalQuery=str("insert into {}.{}({}) values {}".format(destschema,desttable,columns,','.join(values)))
+    return finalQuery
+
 print(os.environ["PG_JDBC_DRIVER_PATH"])#="D:\Learn\ApacheBeam\jar\postgresql-42.2.16.jar"
 
-con=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'),driver_path="D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar").execution_options(isolation_level='AUTOCOMMIT')
-con=con.raw_connection()
-wcon=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'),driver_path="D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar")
-@event.listens_for(wcon,"before_cursor_execute")
-def receive_before_cursor_execute(
-       conn, cursor, statement, params, context, executemany
-        ):
-    print("insideTrigger")
-    if executemany:
-        print("insideexecutemany")
-        cursor.fast_executemany = True
+srccon={"jclassname":'org.postgresql.Driver', 
+"url":'jdbc:postgresql://localhost/sqlalchemyTest', 
+"driver_args":['postgres', 'postgres'], 
+"jars":["D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+,"libs":None}
 
-print('starttime-{}'.format(datetime.datetime.now()))
-for readChunk in pd.read_sql("select randomText,randomNumber from public.testtable",con,chunksize=200000):
-#readChunk=pd.read_sql("select randomText,randomNumber from public.testtable",con,index_col=None,chunksize=None)
-#print(readChunk)
-    print("count:{}".format(len(readChunk)))
-    print('StartLoadChunk-{}'.format(datetime.datetime.now()))
-    #readChunk.to_sql('testtableout1',wcon,schema='public',if_exists='append',index=False)
-    readChunk.to_sql('testtableout10',wcon,schema='public',if_exists='append',index=False,chunksize=15000,method='multi')
-    print('EndLoadChunk-{}'.format(datetime.datetime.now()))
+srcquery="select * from public.testtablelarge limit 1000000"
+
+destcon={"jclassname":'org.postgresql.Driver', 
+"url":'jdbc:postgresql://localhost/sqlalchemyTest', 
+"driver_args":['postgres', 'postgres'], 
+"jars":["D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+,"libs":None}
+
+desttable="testtablelargeoo"
+destschema="public"
+
+with jaydebeapi.connect(**destcon) as wcon:
+    wcon.jconn.setAutoCommit(False)
+    with jaydebeapi.connect(**srccon) as conn:
+        print('starttime-{}'.format(datetime.datetime.now()))
+        try:
+            for readChunk in pd.read_sql(srcquery,conn,chunksize=100000):
+                print('readcomplete-{}'.format(datetime.datetime.now()))
+                columns=','.join(readChunk.columns)
+                values=[]
+                chunksize=20000
+                i=0
+                wcur=wcon.cursor()
+                for row in readChunk.itertuples(index=False):
+                    outrow=','.join(fix_values(row))
+                    values.append(format("({})".format(outrow)))
+                    i=i+1
+                    if i%5000==0:
+                        print("1000chunkready-{}".format(datetime.datetime.now()))
+                    if i==chunksize:
+                        print("chunkready-{}".format(datetime.datetime.now()))
+                        #finalQuery=str("insert into public.testtablelargeo({}) values {}".format(columns,','.join(values)))
+                        wcur.execute(insert_sql_build(values,destschema,desttable,columns))
+                        values=[]
+                        i=0
+                        print("chunkLoaded-{}".format(datetime.datetime.now()))
+                if values !=[]:
+                    print("Lastchunkready-{}".format(datetime.datetime.now()))
+                    #finalQuery=str("insert into public.testtablelargeo({}) values {}".format(columns,','.join(values)))
+                    wcur.execute(insert_sql_build(values,destschema,desttable,columns))
+                    print("LastchunkLoaded-{}".format(datetime.datetime.now()))
+        except jaydebeapi.Error as e:
+            wcon.rollback()
+        finally:
+            wcon.commit()
 print('Endtime-{}'.format(datetime.datetime.now()))
--- python-jdbc-sqlalchemy/src/readPostgresfast.py
+++ python-jdbc-sqlalchemy/src/readPostgresfast.py
@@ -1,67 +1,108 @@
-import sqlalchemy
 import jaydebeapi
-#import sqlalchemy-jdbcapi
 import pandas as pd
 import os
 import datetime
-from sqlalchemy import event
-import io
-from  gzip import GzipFile
 from pyarrow import jvm
 import jpype
 import sys
+import math
 import jaydebeapi
 
+def fix_values(values):
+    outvalues=[]
+    for value in values:
+        if value is None:
+            value='NULL'
 
-def read_sql_pyarrow(query, db_details):
+        if isinstance(value,(int,float)):
+            if math.isnan(value):
+                value='NULL'
+            value=str(value)
+        else:
+            value="'{}'".format(str(value).replace("'","''"))
+        outvalues.append(value)
+    return outvalues
+
+
+def read_sql_pyarrow(query,conn,batchsize=100000):
     from jpype import imports
-    with jaydebeapi._jdbc_connect_jpype('org.postgresql.Driver', 'jdbc:postgresql://localhost/sqlalchemyTest', ['postgres', 'postgres'], ["D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar"],None) as conn:
-    #conn = db_engine.raw_connection()
+    with jaydebeapi._jdbc_connect_jpype(**srccon) as conn:
+        #conn = db_engine.raw_connection()
         from org.apache.arrow.adapter.jdbc import JdbcToArrow
         import org.apache.arrow.memory.RootAllocator as rootallocator
         from org.apache.arrow.adapter.jdbc import JdbcToArrowConfigBuilder
         from org.apache.arrow.adapter.jdbc import JdbcToArrowConfig
-        #import java.sql.DriverManager
-    #from org.apache.arrow.adapter.memory import RootAllocator
-    #dm=java.sql.DriverManager
-        #print("typeConn-{}".format(type(conn)))
-        #print("sysmaxSize-{}".format(sys.maxsize))
+        from org.apache.arrow.adapter.jdbc import JdbcToArrowUtils
+
         ra=rootallocator(sys.maxsize)
-        configbuild=JdbcToArrowConfigBuilder()
-        configbuild.setAllocator(ra)
-        configbuild.setTargetBatchSize(5000)
+        ca=JdbcToArrowUtils.getUtcCalendar()
+        configbuild=JdbcToArrowConfigBuilder(ra,ca,True)
+        configbuild.setTargetBatchSize(batchsize)
         config=configbuild.build()
         cur=conn.createStatement()
         rs=cur.executeQuery(query)
         batch=JdbcToArrow.sqlToArrowVectorIterator(
         rs,
         config)
-        i=0
+
         while batch.hasNext():
-            #combdf=None
             df = jvm.record_batch(batch.next()).to_pandas()
-            #print(len(combdf))
             yield df
+
+def insert_sql_build(values:list,destschema,desttable,columns):
+    finalQuery=str("insert into {}.{}({}) values {}".format(destschema,desttable,columns,','.join(values)))
+    return finalQuery
 
 print(os.environ["PG_JDBC_DRIVER_PATH"])#="D:\Learn\ApacheBeam\jar\postgresql-42.2.16.jar"
 
-con=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest')).execution_options(isolation_level='AUTOCOMMIT')
-wcon=sqlalchemy.create_engine("jdbcapi+pgjdbc://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost', 'sqlalchemyTest'))
-# @event.listens_for(wcon,"before_cursor_execute")
-# def receive_before_cursor_execute(
-#        conn, cursor, statement, params, context, executemany
-#         ):
-#             if executemany:
-#                 cursor.fast_executemany = True
+srccon={"jclassname":'org.postgresql.Driver', 
+"url":'jdbc:postgresql://localhost/sqlalchemyTest', 
+"driver_args":['postgres', 'postgres'], 
+"jars":["D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+,"libs":None}
 
-print('starttime-{}'.format(datetime.datetime.now()))
-#for readChunk in pd.read_sql("select randomText,randomNumber from public.testtable",con,chunksize=100000):
-#readChunk=pd.read_sql("select randomText,randomNumber from public.testtable",con,index_col=None,chunksize=None)
-#print(readChunk)
-    #print("count:{}".format(len(readChunk)))
-    #readChunk.to_sql('testtableout1',wcon,schema='public',if_exists='append',index=False)
-for readChunk in read_sql_pyarrow("select * from public.testtablelarge",con):
-    print("readChunkLen-{}".format(len(readChunk)))
-    print('readcomplete-{}'.format(datetime.datetime.now()))
-    readChunk.to_sql('testtablelargeout',wcon,schema='public',if_exists='append',index=False,chunksize=5000,method='multi')
+srcquery="select * from public.testtablelarge limit 1000000"
+
+destcon={"jclassname":'org.postgresql.Driver', 
+"url":'jdbc:postgresql://localhost/sqlalchemyTest', 
+"driver_args":['postgres', 'postgres'], 
+"jars":["D:\Learn\ApacheBeam\jar\pgjdbc_combined-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+,"libs":None}
+
+desttable="testtablelargeo"
+destschema="public"
+
+with jaydebeapi.connect(**destcon) as wcon:
+    wcon.jconn.setAutoCommit(False)
+    print('starttime-{}'.format(datetime.datetime.now()))
+    try:
+        for readChunk in read_sql_pyarrow(srcquery,srccon):
+            print('readcomplete-{}'.format(datetime.datetime.now()))
+            columns=','.join(readChunk.columns)
+            values=[]
+            chunksize=20000
+            i=0
+            wcur=wcon.cursor()
+            for row in readChunk.itertuples(index=False):
+                outrow=','.join(fix_values(row))
+                values.append(format("({})".format(outrow)))
+                i=i+1
+                if i%5000==0:
+                    print("1000chunkready-{}".format(datetime.datetime.now()))
+                if i==chunksize:
+                    print("chunkready-{}".format(datetime.datetime.now()))
+                    #finalQuery=str("insert into public.testtablelargeo({}) values {}".format(columns,','.join(values)))
+                    wcur.execute(insert_sql_build(values,destschema,desttable,columns))
+                    values=[]
+                    i=0
+                    print("chunkLoaded-{}".format(datetime.datetime.now()))
+            if values !=[]:
+                print("Lastchunkready-{}".format(datetime.datetime.now()))
+                #finalQuery=str("insert into public.testtablelargeo({}) values {}".format(columns,','.join(values)))
+                wcur.execute(insert_sql_build(values,destschema,desttable,columns))
+                print("LastchunkLoaded-{}".format(datetime.datetime.now()))
+    except jaydebeapi.Error as e:
+        wcon.rollback()
+    finally:
+        wcon.commit()
 print('Endtime-{}'.format(datetime.datetime.now()))
